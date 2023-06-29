657ff75dbc2c049d9df141d388987db198be6d93
MikheevVitaliy <mikheev.vitaliy87@gmail.com>
2023-06-29 21:47:11 +0300

    Update BackButton.jsx

--- src/components/BackButton/BackButton.jsx
+++ src/components/BackButton/BackButton.jsx
@@ -1,4 +1,5 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+// import { Link, useLocation } from 'react-router-dom';
 import { BsArrowLeft } from 'react-icons/bs';
 
 import css from './BackButton.module.css';
@@ -7,11 +8,13 @@ export const BackButton = ({ children }) => {
   /*  Функция 'useLocation()' получает текущий URL страници и сохраняет в переменной 'location'. Далее с оператором '?'
      обращаемся до свойств 'state' с переменной 'location',
       которая содержит информацию о предыдущей странице. Если такая информация есть, значит получаем URL предыдушей страници и сохраняем в переменную 'backLinkHref'. Если информации нет, то устанавливаем URL страници по умолчанию на ("/"). */
-  const location = useLocation();
-  const backLinkHref = location.state?.from ?? '/';
+
+  // const location = useLocation();
+  // const backLinkHref = location.state?.from ?? '/';
 
   return (
-    <Link className={css.back__link} to={backLinkHref}>
+    <Link className={css.back__link} to={'/'}>
+      {/* <Link className={css.back__link} to={backLinkHref}> */}
       <BsArrowLeft size={16} />
       {children}
     </Link>
